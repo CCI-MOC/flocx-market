55a193a4a4e0e2928b07e18ce2bc804d33342efc
DanNiESh <dannis@andrew.cmu.edu>
2019-06-18 12:05:17 -0400

    create raw sql in sqlalchemy

--- flocx_market/db/api.py
+++ flocx_market/db/api.py
@@ -2,7 +2,7 @@ import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flocx_market.db import models as db_models
-
+from sqlalchemy.sql import text
 
 _ENGINE = None
 _SESSION_MAKER = None
@@ -30,9 +30,28 @@ def get_session():
     engine = get_engine()
     Session = get_session_maker(engine)
     session = Session()
-
+    create_sample_data(engine)
     return session
 
+def create_sample_data(engine):
+    con = engine.connect()
+    data =  { "id":1,\
+        "marketplace_offer_id":"b711b1ca-a77e-4392-a9b5-dc84c4f469ac",\
+        "provider_id":"b9752cc0-9bed-4f1c-8917-12ade7a6fdbe",\
+        "creator_id":"Alice1992",\
+        "marketplace_date_created":"2016-07-16T19:20:30",\
+        "status":"available",\
+        "server_name:":"Dell380",\
+        "start_time":"2016-07-16T19:20:30",\
+        "end_time": "2016-08-16T19:20:30",\
+        "duration":164,\
+        "server_config":'{"new attribute XYZ": "This is just a sample list of free-form attributes used for describing a server."\
+        ,"cpu_type": "Intel Xeon","cores": 16,"ram_gb": 512,"storage_type": "samsung SSD", "storage_size_gb": 204}',
+        "cost":11 }
+    con.execute("DELETE FROM offer WHERE id = 1")
+    statement = text("""INSERT INTO offer VALUES(:id, :marketplace_offer_id, :provider_id, :creator_id, \
+        :marketplace_date_created, :status, "Dell380", :start_time, :end_time, :duration, :server_config, :cost)""")
+    con.execute(statement, **data)
 #3.adding and updating object
 class Connection(object):
 
