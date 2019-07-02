98eee4456f1494bec138e5703e88dc2e18620b16
Lars Kellogg-Stedman <lars@redhat.com>
2019-07-01 20:58:07 -0400

    Teach test_commit.py about --message-from-file
    This introduces the --message-from-file option to the commit test,
    which allows us to call the commit test from a git `commit-msg` hook.
    
    Part of #29

--- flocx_market/tests/commit/test_commit_message.py
+++ flocx_market/tests/commit/test_commit_message.py
@@ -7,9 +7,6 @@ import subprocess
 import git
 import pytest
 
-RE_PARSE_NAME = re.compile(r'(?P<name>.*) <(?P<email>[^>]+)> '
-                           r'(?P<timestamp>\d+) (?P<tz>[-\d]+)')
-
 
 @pytest.fixture
 def commit_id():
@@ -24,17 +21,40 @@ def repo():
 
 
 @pytest.fixture
-def commit(repo, commit_id):
-    '''Convert the parametrized commit_id into a Commit object'''
-    return repo.commit(commit_id)
+def commit_message(repo, commit_id, commit_message_file):
+    '''Return the commit message to be checked.
+
+    If --message-from-file was specified on the command line, read the
+    commit message from the named file. Otherwise, use the `git` module to
+    resolve the named commit into a commit object and return the associated
+    message.'''
+
+    if commit_id == '__file__':
+        with open(commit_message_file) as fd:
+            message = '\n'.join(
+                line.strip() for line in fd.readlines()
+                if not line.startswith('#')
+            )
+            return message
+    else:
+        return repo.commit(commit_id).message
 
 
 @pytest.fixture
 def max_subject_length():
+    '''Allow the maximum subject length to be configured in the environment.
+
+    People somtimes have opinions on this sort of thing, so allow this
+    value to be configured via an environment variable rather than
+    requiring changes to the code.'''
     return int(os.environ.get('COMMIT_MAX_SUBJECT_LENGTH', 60))
 
 
 def get_commits_from_pr():
+    '''Generate list of commits to check from TRAVIS_COMMIT_RANGE.
+
+    This uses `git rev-list` to resolve the given commit range into
+    a list of commits.'''
     commit_range = os.environ.get('TRAVIS_COMMIT_RANGE')
     out = subprocess.check_output(['git', 'rev-list', commit_range])
     out = out.decode('utf-8')
@@ -46,19 +66,24 @@ def get_commits_from_pr():
 def pytest_generate_tests(metafunc):
     '''Get list of commits involved in current pull request'''
 
-    if os.environ.get('TRAVIS_PULL_REQUEST', 'false') != 'false':
+    commit_message_file = metafunc.config.getoption('--message-from-file')
+
+    if commit_message_file:
+        commits = ['__file__']
+    elif os.environ.get('TRAVIS_PULL_REQUEST', 'false') != 'false':
         commits = get_commits_from_pr()
     elif 'TRAVIS_COMMIT' in os.environ:
         commits = [os.environ.get('TRAVIS_COMMIT')]
     else:
         commits = ['HEAD']
 
+    metafunc.parametrize('commit_message_file', [commit_message_file])
     metafunc.parametrize('commit_id', commits)
 
 
-def test_commit_subject_and_body(commit):
+def test_commit_subject_and_body(commit_message):
     '''Verify that a commit has a subject and body separated by a blank line'''
-    message = commit.message.splitlines()
+    message = commit_message.splitlines()
     assert len(message) > 2, (
         'Commit message must include both subject and body')
 
@@ -66,42 +91,42 @@ def test_commit_subject_and_body(commit):
         'Commit message must have a blank line after the subject')
 
 
-def test_commit_subject_no_period(commit):
+def test_commit_subject_no_period(commit_message):
     '''Verify that a commit message does not end with a "."'''
-    subject = commit.message.splitlines()[0]
+    subject = commit_message.splitlines()[0]
     assert not subject.endswith('.'), (
         'Commit messages should not end with a period (".")')
 
 
-def test_commit_subject_length(commit, max_subject_length):
+def test_commit_subject_length(commit_message, max_subject_length):
     '''Verify the length of the commit subject
 
     This will fail if the subject is longer than
     COMMIT_MAX_SUBJECT_LENGTH, if defined, otherwise
     60 characters.'''
-    subject = commit.message.splitlines()[0]
+    subject = commit_message.splitlines()[0]
     assert len(subject) < max_subject_length, (
         'Commit subject should be descriptive but less than '
         '{} characters'.format(max_subject_length))
 
 
-def test_commit_line_length(commit):
+def test_commit_line_length(commit_message):
     '''Verify the length of the commit subject
 
     This will fail if lines in the message are not
     wrapped to < 75 characters.'''
-    message = commit.message.splitlines()
-    assert len(message) > 1
+    message = commit_message.splitlines()
 
-    for line in message:
-        assert len(line) < 75, (
-            'Commit message body should be wrapped at 75 characters')
+    if len(message) > 2:
+        for line in message[2:]:
+            assert len(line) < 75, (
+                'Commit message body should be wrapped at 75 characters')
 
 
-def test_commit_has_reference(commit):
+def test_commit_has_reference(commit_message):
     '''Verify that a commit includes a bug/issue/task/etc reference'''
-    has_gh_issue_refs = re.findall(r'#\d+', commit.message)
-    has_taiga_issue_refs = re.findall(r'TG-\d+', commit.message)
+    has_gh_issue_refs = re.findall(r'#\d+', commit_message)
+    has_taiga_issue_refs = re.findall(r'TG-\d+', commit_message)
 
     assert has_gh_issue_refs or has_taiga_issue_refs, (
         'Commit message does not reference related issue/task/user story. '
