cbef986c0bad37bf463d4fd5d9c894e37a6e806c
Lars Kellogg-Stedman <lars@redhat.com>
2019-07-09 15:31:06 -0400

    Correct tests introduced in #41
    Pytest discovers tests automatically by looking for files named
    `test_<something>.py`. The tests introduced in #41 were not using this
    naming scheme and thus were not discoverable by pytest.  This commit
    renames the test file and rewrites the tests using pytest-style tests,
    which result in slightly simpler code.
    
    Closes #47

---
+++ flocx_market/tests/unit/api/test_app.py
@@ -0,0 +1,32 @@
+import flask
+import json
+import pytest
+
+import flocx_market.api.app
+import flocx_market.conf
+
+CONF = flocx_market.conf.CONF
+
+
+@pytest.fixture
+def test_app():
+    app = flocx_market.api.app.create_app(app_name="test").test_client()
+    app.testing = True
+    return app
+
+
+def test_root_status_code(test_app):
+    response = test_app.get("/", follow_redirects=True)
+    assert response.status_code == 200
+
+
+def test_root_data(test_app):
+    response = test_app.get("/", follow_redirects=True)
+    flocx_market_url = CONF.api.host_ip + ":" + str(CONF.api.port)
+    version = {"versions":
+               {"values": [{"status": "in progress",
+                            "updated": "2019-07-02T00:00:00Z",
+                            "media-types": [{"base": "application/json", "name": "flocx-market"}],
+                            "links": [{"href": flocx_market_url, "rel": "self"}]}]
+                }}
+    assert json.loads(response.data) == version
