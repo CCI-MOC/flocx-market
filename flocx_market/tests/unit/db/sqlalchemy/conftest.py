2e9de561a4a7824482553e051e40e854e0a4d3a5
Lars Kellogg-Stedman <lars@redhat.com>
2019-07-10 12:19:00 -0400

    Clean up offer unit tests
    The unit tests introduced in #30 were incorrectly named.  This commit
    places them in the correct location and ensures we have tests for save
    and delete.
    
    Closes #53
    
    x-branch-name: cleanup/unit_tests

---
+++ flocx_market/tests/unit/db/sqlalchemy/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+
+from flocx_market.common.service import prepare_service
+from flocx_market.conf import CONF
+from flocx_market.api.app import create_app
+from flocx_market.db.orm import orm
+from flocx_market.db.sqlalchemy.offer_api import OfferApi
+
+
+class test_app_config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    TESTING = True
+
+
+@pytest.fixture(scope='session')
+def app():
+    CONF.clear()
+    prepare_service()
+    CONF.database.connection = 'sqlite:///:memory:'
+    app = create_app('testing')
+    orm.init_app(app)
+    ctx = app.app_context()
+    ctx.push()
+    yield app
+    ctx.push()
+
+
+@pytest.fixture()
+def db(app):
+    orm.create_all()
+    yield orm
+    orm.drop_all()
+
+
+@pytest.fixture()
+def session(db):
+    session = db.session()
+    yield session
+    if not session.transaction.is_active:
+        session.rollback()
