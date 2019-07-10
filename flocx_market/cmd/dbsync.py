a019e6ea7e520c53cfe1987af28d9d20b1a87c28
Tzu-Mainn Chen <tzumainn@redhat.com>
2019-07-10 18:08:26 +0000

    Fixes dbsync script
    This change updates the dbsync script to call prepare_services to
    ensure that oslo configurations are set up, and no longer  call the
    non-existent create_app() function from flocx_market.cmd.api
    
    Closes #58

--- flocx_market/cmd/dbsync.py
+++ flocx_market/cmd/dbsync.py
@@ -1,8 +1,13 @@
-from flocx_market.cmd import api
+import sys
+
+from flocx_market.api import app
+from flocx_market.common import service as flocx_market_service
 from flocx_market.db.orm import orm
 
 
 def main():
-    application = api.create_app()
-    orm.init_app(application)
-    orm.create_all()
+    flocx_market_service.prepare_service(sys.argv)
+    application = app.create_app("dbsync")
+    with application.app_context():
+        orm.init_app(application)
+        orm.create_all()
