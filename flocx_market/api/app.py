be6acab941e47c44b8faa08490d4d6208f9c5e81
Lars Kellogg-Stedman <lars@redhat.com>
2019-07-10 12:12:21 -0400

    Handle root url using a flask Resource
    This uses the same mechanism for handling requests to `/` that we use
    for handling requests to `/offer`.
    
    Closes #51
    
    x-branch-name: root_resource

--- flocx_market/api/app.py
+++ flocx_market/api/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flocx_market.api.offer import Offer
-from flask import Blueprint
-from flask import jsonify
+from flocx_market.api.root import Root
 import flocx_market.conf
 from oslo_config import cfg
 
@@ -10,23 +9,12 @@ from oslo_config import cfg
 CONF = flocx_market.conf.CONF
 
 
-def index():
-    flocx_market_url = CONF.api.host_ip + ':' + str(CONF.api.port)
-    version = {"versions":
-               {"values": [{"status": "in progress",
-                            "updated": "2019-07-02T00:00:00Z",
-                            "media-types": [{"base": "application/json", "name": "flocx-market"}],
-                            "links": [{"href": flocx_market_url, "rel": "self"}]}]
-                }}
-    return jsonify(version)
-
-
 def create_app(app_name):
     app = Flask(app_name)
     app.config['SQLALCHEMY_DATABASE_URI'] = CONF.database.connection
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = CONF.flask.SQLALCHEMY_TRACK_MODIFICATIONS
     app.config['PROPAGATE_EXCEPTIONS'] = CONF.flask.PROPAGATE_EXCEPTIONS
-    app.route('/')(index)
     api = Api(app)
     api.add_resource(Offer, '/offer', '/offers/<string:marketplace_offer_id>')
+    api.add_resource(Root, '/')
     return app
---
+++ flocx_market/api/root.py
@@ -0,0 +1,31 @@
+from flask import jsonify
+from flask_restful import Resource
+from flocx_market.db.sqlalchemy.offer_api import OfferApi
+import flocx_market.conf
+
+CONF = flocx_market.conf.CONF
+
+
+class Root(Resource):
+    @classmethod
+    def get(cls):
+        flocx_market_url = CONF.api.host_ip + ':' + str(CONF.api.port)
+        version = {
+            "versions": {
+                "values": [{
+                    "status":
+                    "in progress",
+                    "updated":
+                    "2019-07-02T00:00:00Z",
+                    "media-types": [{
+                        "base": "application/json",
+                        "name": "flocx-market"
+                    }],
+                    "links": [{
+                        "href": flocx_market_url,
+                        "rel": "self"
+                    }]
+                }]
+            }
+        }
+        return jsonify(version)
